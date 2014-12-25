11b28802388fae6c11a1f86fcc7728d94c40e391
Krzysztof Grodzicki <krzysztof.grodzicki@embriq.no>
2014-12-25 20:42:24 +0100

    P11

--- src/main/scala/no/embriq/scala/exercises/Excersise.scala
+++ src/main/scala/no/embriq/scala/exercises/Excersise.scala
@@ -124,7 +124,10 @@ object Excersise {
   //	Example: 
   //		scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   //		res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
-  def encodeModified[A](ls: List[A]): List[Any] = ???
+  def encodeModified[A](ls: List[A]): List[Any] = pack(ls) flatMap {
+    case List(a) => List(a)
+    case list: List[A] => List((list.size, list.head))
+  }
 
   // P12 (Middels vanskelig) Decode a run-length encoded list.
   //Given a run-length code list generated as specified in problem P10, construct its uncompressed version. 
